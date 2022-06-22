89b70951e5c3dc5fceb471056466a97c15b537c5
danugarri <danugarri@hotmail.com>
2022-06-22 15:14:13 +0200

    deleting comments

--- src/app/shared/helpers.js
+++ src/app/shared/helpers.js
@@ -1,11 +1,5 @@
 export const checkValues = (...args) => {
-  //   for (let i = 0; i < args.length; i++) {
-  //     if (args[i] === '' || args[i] === 0) {
-  //       return false;
-  //     } else {
-  //       return true;
-  //     }
-  //   }
   const result = args.some((param) => param === '');
+
   return result;
 };
