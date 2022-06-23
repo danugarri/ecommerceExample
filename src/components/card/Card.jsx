a2ac9b6a330b42232866a561e304558b7c3bfffb
danugarri <danugarri@hotmail.com>
2022-06-23 02:06:50 +0200

    adding product name dynamiclly

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,20 +7,19 @@ import { useDispatch } from 'react-redux/es/exports';
 import { shoppingCartAction } from '../../app/actions/shoppingCartAction';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 
-
 export const Card = ({ setCounter }) => {
   const [selectedColor, setSelectedColor] = useState('');
   const [selectedSize, setSelectedSize] = useState('');
   const [selectedQuantity, setSelectedQuantity] = useState('');
   const [isFulfilled, setIsFulfilled] = useState(false);
-  const fetchedData = useSelector(state => state.getData.entities);
-  const {code,name,stock}= fetchedData;
+  const fetchedData = useSelector((state) => state.getData.entities);
+  const { code, name, stock } = fetchedData;
   const dispatch = useDispatch();
   const HandleClick = () => {
-    const formattedMessage = `Has añadido ${selectedQuantity} unidad/des de la talla ${selectedSize} y color ${selectedColor}`;
+    const formattedMessage = `Has añadido ${selectedQuantity} unidad/des de ${name} de la talla ${selectedSize} y color ${selectedColor}`;
     if (!isFulfilled) {
       swal(formattedMessage, '', 'success');
-      dispatch(shoppingCartAction('product1', selectedQuantity, selectedColor, selectedSize));
+      dispatch(shoppingCartAction(name, selectedQuantity, selectedColor, selectedSize));
       setCounter((prev) => prev + 1);
     }
     setSelectedColor('');
@@ -37,13 +36,21 @@ export const Card = ({ setCounter }) => {
     <section>
       <h3>{name}</h3>
       <h3>499€</h3>
-      <section id= 'features'>
-        <div>Availability: <span>{stock.stockLevelStatus}</span></div>
-        <div>Product code: <span>{code}</span></div>
-        <div>Tags: <span>Woman</span></div>
+      <section id='features'>
+        <div>
+          Availability: <span>{stock.stockLevelStatus}</span>
+        </div>
+        <div>
+          Product code: <span>{code}</span>
+        </div>
+        <div>
+          Tags: <span>Woman</span>
+        </div>
       </section>
-      <p id= 'description'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus qui culpa at nesciunt ipsum dicta reprehenderit odio delectus dolorem nisi! Aliquam autem fugit quaerat neque a consectetur nesciunt quae quia.
+      <p id='description'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus qui culpa at nesciunt
+        ipsum dicta reprehenderit odio delectus dolorem nisi! Aliquam autem fugit quaerat neque a
+        consectetur nesciunt quae quia.
       </p>
       <Selects
         setCounter={setCounter}
