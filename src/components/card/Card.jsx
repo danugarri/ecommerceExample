41897dc19958e1a975a1ee29cf66f0f13bcdeb2d
danugarri <danugarri@hotmail.com>
2022-06-23 02:02:49 +0200

    using hook to bring data

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,12 +5,16 @@ import { checkValues } from '../../app/shared/helpers';
 import swal from 'sweetalert';
 import { useDispatch } from 'react-redux/es/exports';
 import { shoppingCartAction } from '../../app/actions/shoppingCartAction';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+
 
 export const Card = ({ setCounter }) => {
   const [selectedColor, setSelectedColor] = useState('');
   const [selectedSize, setSelectedSize] = useState('');
   const [selectedQuantity, setSelectedQuantity] = useState('');
   const [isFulfilled, setIsFulfilled] = useState(false);
+  const fetchedData = useSelector(state => state.getData.entities);
+  const {code,name,stock}= fetchedData;
   const dispatch = useDispatch();
   const HandleClick = () => {
     const formattedMessage = `Has añadido ${selectedQuantity} unidad/des de la talla ${selectedSize} y color ${selectedColor}`;
@@ -31,12 +35,12 @@ export const Card = ({ setCounter }) => {
 
   return (
     <section>
-      <h3>Product Title</h3>
+      <h3>{name}</h3>
       <h3>499€</h3>
       <section id= 'features'>
-        <label>Availability: </label><span>In stock</span>
-        <label>Product code: </label><span>#4444</span>
-        <label>Tags: </label><span>Woman</span>
+        <div>Availability: <span>{stock.stockLevelStatus}</span></div>
+        <div>Product code: <span>{code}</span></div>
+        <div>Tags: <span>Woman</span></div>
       </section>
       <p id= 'description'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus qui culpa at nesciunt ipsum dicta reprehenderit odio delectus dolorem nisi! Aliquam autem fugit quaerat neque a consectetur nesciunt quae quia.
