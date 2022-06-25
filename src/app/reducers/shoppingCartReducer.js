7c58de6e2875b5f103a4ecb936796944ddbef152
danugarri <danugarri@hotmail.com>
2022-06-25 13:48:20 +0200

    improved reducer

--- src/app/reducers/shoppingCartReducer.js
+++ src/app/reducers/shoppingCartReducer.js
@@ -1,14 +1,19 @@
 import { types } from '../types/types';
 
+const order = {
+  'Product Name': '',
+  Quantity: '',
+  Size: '',
+  Color: '',
+};
 const initialState = {
-  orders: [],
+  orders: [order],
 };
 export const shoppingCartReducer = (state = initialState, action) => {
   if (action.type === types.shoppingCart) {
     return Object.assign({}, state, {
-        orders: state.orders.concat(action.payload)
-      });
-   
+      orders: state.orders.concat(action.payload),
+    });
   }
   return state;
 };
